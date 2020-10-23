9931874457420af7a97cfa2b9591c5c735fc55ce
truetranslator <55185307+truetranslator@users.noreply.github.com>
2020-10-23 16:41:00 +0300

    Initial Commit

---
+++ src/test/java/bg/softuni/moiterecepti/MoiteReceptiApplicationTests.java
@@ -0,0 +1,13 @@
+package bg.softuni.moiterecepti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoiteReceptiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
